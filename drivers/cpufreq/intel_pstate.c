a62c6df14573761a83ab60643d2e9e5384ce5129
Giovanni Gherdovich <ggherdovich@suse.cz>
2021-11-26 17:13:21 +0100

    cpufreq: intel_pstate: Add Icelake servers support in no-HWP mode
    BugLink: https://bugs.launchpad.net/bugs/1952234
    
    Users may disable HWP in firmware, in which case intel_pstate wouldn't load
    unless the CPU model is explicitly supported.
    
    Add ICELAKE_X to the list of CPUs that can register intel_pstate while not
    advertising the HWP capability. Without this change, an ICELAKE_X in no-HWP
    mode could only use the acpi_cpufreq frequency scaling driver.
    
    See also commit d8de7a44e11f ("cpufreq: intel_pstate: Add Skylake servers
    support").
    
    Signed-off-by: Giovanni Gherdovich <ggherdovich@suse.cz>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (backported from commit fbdc21e9b038d00d0d56fa4e0f7701d42ae08f00)
    Signed-off-by: Bartlomiej Zolnierkiewicz <bartlomiej.zolnierkiewicz@canonical.com>
    Acked-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
    Acked-by: Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -1644,6 +1644,7 @@ static const struct x86_cpu_id intel_pstate_cpu_ids[] = {
 	ICPU(INTEL_FAM6_XEON_PHI_KNM,		knl_funcs),
 	ICPU(INTEL_FAM6_ATOM_GOLDMONT,		bxt_funcs),
 	ICPU(INTEL_FAM6_ATOM_GOLDMONT_PLUS,     bxt_funcs),
+	ICPU(INTEL_FAM6_ICELAKE_X,		core_funcs),
 	{}
 };
 MODULE_DEVICE_TABLE(x86cpu, intel_pstate_cpu_ids);
